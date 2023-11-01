f44a8e0957132fdfb6160c760ac7de1c75565fc1
vladpavlovhz <pavl0008@hz.nl>
2023-11-01 18:33:41 +0100

    Deleted login

--- events/src/App.svelte
+++ events/src/App.svelte
@@ -1,13 +1,11 @@
 <script>
 	import Dashboard from './Dashboard.svelte';
-  import Login from './Login.svelte';
   import Eventsidebar from './EventSidebar.svelte';
   </script>
   
   <main>
 	<h1>Welcome to CUBY Stress Tracker</h1>
 	<Dashboard />
-	<Login />
 	<Eventsidebar />
   </main>
   
